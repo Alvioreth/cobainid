32ddf3b96729778982966f9c50b2a3b0eeff1a2e
Alvioreth <wahyusatrianusan124@gmail.com>
2026-05-09 03:10:28 +0700

    Update config.js

--- config.js
+++ config.js
@@ -5,6 +5,6 @@
    3) Tempel URL Web App di APPS_SCRIPT_URL di bawah ini
    ===================================================== */
 window.COBAIN_CONFIG = {
-  APPS_SCRIPT_URL: "https://script.google.com/macros/s/AKfycbySbkSrA4nIL2yn0MmHOf2_rThVfzwmYmBcpoWISNM_Jq3bTuXlDLLJybgIl0cPduYYKA/exec",
+  APPS_SCRIPT_URL: "https://script.google.com/macros/s/AKfycbws5_XJ6TLYnIj0chnO6Eei8EfEhEOzP4BsoL348E3P2N3gcZ_J1L8MhOSyTFf0vj9gHQ/exec",
   HOST_CODE: "COBAINHOST"
 };
