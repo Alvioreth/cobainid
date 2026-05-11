96ca88ad18154c6549a6af61af3ee0e4dacbfd13
Alvioreth <wahyusatrianusan124@gmail.com>
2026-05-11 23:36:48 +0700

    Update config.js

--- config.js
+++ config.js
@@ -3,6 +3,6 @@
    APPS_SCRIPT_URL harus memakai Web App URL yang berakhiran /exec.
    ===================================================== */
 window.COBAIN_CONFIG = {
-  APPS_SCRIPT_URL: "https://script.google.com/macros/s/AKfycbwX-beZdWsucvDrUfvExHB9UnVeGFv0V9mdo90ShXMZn-fgh1zeA7AufAFOg_d6CNJRiQ/exec",
+  APPS_SCRIPT_URL: "https://script.google.com/macros/s/AKfycbwYebvn73bcHPbP0MuwJnUb7OfUuDdJApPDJmwl9zZ2d6UxclH6Hs1S48dSaFtr54raCQ/exec",
   HOST_CODE: "COBAINHOST"
 };
